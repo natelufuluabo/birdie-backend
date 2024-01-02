f95322fbf8f8d8643d1014c589b9b399ef0c2be5
aircongo <natelufuluabo@yahoo.ca>
2024-01-02 07:41:58 -0500

    created gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { FirebaseModule } from './firebase/firebase.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { SocketModule } from './socket/socket.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ cache: true }), FirebaseModule],
+  imports: [ConfigModule.forRoot({ cache: true }), FirebaseModule, SocketModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/socket/socket.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { SocketGateway } from './socket/socket.gateway';
+
+@Module({
+  providers: [SocketGateway]
+})
+export class SocketModule {}
