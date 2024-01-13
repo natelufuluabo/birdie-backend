cf74a72505ed750c66c683c37c5d6dbeb8bf6a8d
aircongo <natelufuluabo@yahoo.ca>
2024-01-13 08:47:22 -0500

    implemented function that will used to create custom token

--- server/functions/index.js
+++ server/functions/index.js
@@ -7,13 +7,26 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const app = require("../app");
+const serviceAccount = require("./serviceAccount.json");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.createCustomToken = functions.https.onRequest(
+    async (req, res) => {
+        try {
+            const userId = req.body.userId;
+            const customToken = await admin.auth().createCustomToken(userId);
+            console.log(customToken);
+            res.status(200).json({ customToken });
+        } catch (error) {
+            console.log("Error creating custom token", error);
+        }
+    },
+);
+
+exports.myFunction = functions.https.onRequest(app);
