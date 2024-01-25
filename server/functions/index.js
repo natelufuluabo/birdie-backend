1efc163dde25792863326a7913f50037cd4f3e9c
aircongo <natelufuluabo@yahoo.ca>
2024-01-24 19:36:16 -0500

    upgraded function to gen 2 of cloud functions and redeployed it

--- server/functions/index.js
+++ server/functions/index.js
@@ -6,9 +6,7 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-const express = require("express");
-const app = express();
-const functions = require("firebase-functions");
+const {onRequest} = require("firebase-functions/v2/https");
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccount.json");
 
@@ -16,18 +14,15 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-exports.createCustomToken = functions.https.onRequest(
-    async (req, res) => {
-      try {
-        const userId = req.body.userId;
-        const customToken = await admin.auth().createCustomToken(userId);
-        console.log(customToken);
-        res.status(200).json({customToken});
-      } catch (error) {
-        console.log("Error creating custom token", error);
-        res.status(500).json({error: "Internal Server Error"});
-      }
-    },
+exports.createSignInCustomToken = onRequest(async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const customToken = await admin.auth().createCustomToken(userId);
+    console.log(customToken);
+    res.status(200).json({customToken});
+  } catch (error) {
+    console.log("Error creating custom token", error);
+    res.status(500).json({error: "Internal Server Error"});
+  }
+},
 );
-
-exports.myFunction = functions.https.onRequest(app);
